02cb9959c4512874064963a487f7aae493aca839
Lorraine McGonigle <mcgonigle.lorraine@gmail.com>
2018-08-01 14:31:18 +0100

    adding question three

---
+++ questionThree.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class questionThree {
+
+    public static void main(String[] args) {
+        String sentence = "Mr John Smith";
+        String finalString = replace(sentence);
+        System.out.println(finalString);
+        
+    }
+    
+    public static String replace(String s){
+        //create a char array from the string
+        char[] sArray = s.toCharArray();
+        for(int x = 0; x < sArray.length; x++){
+            if(sArray[x] == ' '){
+                sArray[x] = '%';
+            }
+        }
+        //change it back to a string
+        String newString = new String(sArray);
+        String finalString = newString.replaceAll("%", "%20");
+        return finalString;
+    }
+  }  
+    
